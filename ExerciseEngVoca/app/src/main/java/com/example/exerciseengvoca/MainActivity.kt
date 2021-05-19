1b05364e9fb2c41cf65fda40247c93494e60f4ac
Your Name <you@your-domain.com>
2021-05-20 02:05:48 +0900

    no message

--- ExerciseEngVoca/app/src/main/java/com/example/exerciseengvoca/MainActivity.kt
+++ ExerciseEngVoca/app/src/main/java/com/example/exerciseengvoca/MainActivity.kt
@@ -1,11 +1,9 @@
-package com.example.example1
+package com.example.exerciseengvoca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.ScriptGroup
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: Main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
