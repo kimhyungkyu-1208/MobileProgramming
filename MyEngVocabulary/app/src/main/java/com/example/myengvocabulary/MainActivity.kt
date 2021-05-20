055356b167a6408b05ad0c6c7b73063bdf0b5dd9
Your Name <you@your-domain.com>
2021-05-20 17:30:49 +0900

    zzzz

--- MyEngVocabulary/app/src/main/java/com/example/myengvocabulary/MainActivity.kt
+++ MyEngVocabulary/app/src/main/java/com/example/myengvocabulary/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.myengvocabulary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewPager2: ViewPager2
+    lateinit var tabLatout: TabLayout
+    lateinit var deckFab: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
