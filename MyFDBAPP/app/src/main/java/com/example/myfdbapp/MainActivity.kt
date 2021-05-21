9939037568be1a4a88dc9466ecb2010791d470fd
Your Name <you@your-domain.com>
2021-05-21 23:28:04 +0900

    no message

--- MyFDBAPP/app/src/main/java/com/example/myfdbapp/MainActivity.kt
+++ MyFDBAPP/app/src/main/java/com/example/myfdbapp/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.myfdbapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myfdbapp.databinding.ActivityMainBinding
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var layoutManager: LinearLayoutManager
+    lateinit var adapter: MyProductAdapter
+    lateinit var rdb:DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,14 +22,45 @@ class MainActivity : AppCompatActivity() {
         init()
     }
 
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
     private fun init() {
         layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rdb = FirebaseDatabase.getInstance().getReference("Products/items")
+        val query = rdb.limitToLast(50) //질의문
+        val option = FirebaseRecyclerOptions.Builder<Product>()
+            .setQuery(query, Product::class.java).build()
+        adapter = MyProductAdapter(option)
+        adapter.itemClickListener = object : MyProductAdapter.OnItemClickListener{
+            override fun onItemClick(view: View, position: Int) {
+                binding.apply{
+                    pIdEdit.setText(adapter.getItem(position).PId.toString())
+                    pNameEdit.setText(adapter.getItem(position).pName.toString())
+                    pQuantityEdit.setText(adapter.getItem(position).pQuantity.toString())
+                }
+            }
+
+        }
 
         binding.apply {
             recyclerView.layoutManager = layoutManager
+            recyclerView.adapter = adapter
 
             insertbtn.setOnClickListener {
 
+                val item = Product(pIdEdit.text.toString().toInt(),
+                    pNameEdit.text.toString(),
+                    pQuantityEdit.text.toString().toInt())
+                rdb.child(pIdEdit.text.toString()).setValue(item)
+                clearInput()
             }
 
             findbtn.setOnClickListener {
--- MyFDBAPP/app/src/main/java/com/example/myfdbapp/MyProductAdapter.kt
+++ MyFDBAPP/app/src/main/java/com/example/myfdbapp/MyProductAdapter.kt
@@ -1,12 +1,40 @@
 package com.example.myfdbapp
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myfdbapp.databinding.RowBinding
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
 
-class MyProductAdapter (options: FirebaseRecyclerOptions<Product>):FirebaseRecyclerAdapter<Product, MyProductAdapter.ViewHolder>(options){
+class MyProductAdapter (options: FirebaseRecyclerOptions<Product>):
+    FirebaseRecyclerAdapter<Product, MyProductAdapter.ViewHolder>(options){
+
+    interface OnItemClickListener{
+        fun onItemClick(view: android.view.View, position: Int)
+    }
+
+    var itemClickListener:OnItemClickListener?=null
 
     inner class ViewHolder(val binding: RowBinding):RecyclerView.ViewHolder(binding.root){
+        init{
+            binding.root.setOnClickListener {
+                itemClickListener!!.onItemClick(it, adapterPosition)
+            }
+        }
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = RowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, model: Product) {
+        holder.binding.apply {
+            productid.text = model.PId.toString()
+            productname.text = model.pName.toString()
+            productquantity.text = model.pQuantity.toString()
+        }
     }
 
 
